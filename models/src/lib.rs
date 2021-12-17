5bf23cc2978e33fdad4040591cba5a86e3b4fe5b
Akihito Kirisaki <akirisaki@seqsense.com>
2021-12-17 15:12:11 +0900

    feat: Define models

--- models/src/lib.rs
+++ models/src/lib.rs
@@ -1,8 +1,55 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::{sync::Arc, collections::HashMap};
+
+use async_graphql::{Object, Context, futures_util::lock::Mutex, Schema, EmptyMutation, EmptySubscription};
+
+pub type UsersSchema= Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+
+#[derive(Clone)]
+pub struct User {
+    id: String,
+    name: String,
+    note: Option<String>,
+}
+
+#[Object]
+impl User {
+    async fn id(&self) -> &str {
+        &self.id
+    }
+    async fn name(&self) -> &str {
+        &self.name
+    }
+    async fn note(&self) -> Option<&str> {
+        match &self.note {
+            Some(x) => Some(&x),
+            None => None
+        }
     }
 }
+
+pub struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn users(&self, ctx: &Context<'_>) -> Vec<User> {
+        ctx
+            .data_unchecked::<Arc<Mutex<Vec<User>>>>()
+            .lock()
+            .await
+            .iter()
+            .cloned()
+            .collect()
+    }
+
+    async fn user_by_id(&self, ctx: &Context<'_>, id: String) -> Option<User> {
+        ctx
+            .data_unchecked::<Arc<Mutex<Vec<User>>>>()
+            .lock()
+            .await
+            .iter()
+            .cloned()
+            .find(|x| x.id == id)
+    }
+}
+
+
